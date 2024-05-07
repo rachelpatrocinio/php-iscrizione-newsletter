40a267d6c4d6213c691fb8c555f5e7ab11170f60
rachelpatrocinio <rachelannepatrocinio@gmail.com>
2024-05-07 14:37:10 +0200

    Refactor: printing on page of $validation adding a condition

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-$email = $_POST["email"] ?? null;
+$email = $_POST["email"] ?? '';
 // var_dump($email);
 
 require_once __DIR__ . '/utilities.php';
@@ -18,16 +18,16 @@ $validation = isCorrect($email);
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>php-iscrizione-newsletter</title>
 </head>
-<body>
+<body class="p-5">
     <div class="container">
-        <div class="row p-5">
+        <div class="row">
             <form action="" method="POST">
                 <label for="email">E-mail</label>
                 <input type="text" name="email">
 
                 <button class="btn btn-primary">SEND</button>
             </form>
-            <p><?= $validation;?></p>
+            <p><?php if($email !== ''){echo $validation;}?></p>
         </div>
     </div>
     
